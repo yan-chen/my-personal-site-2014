8d7120efc4e24eeae74150e9deedbcb1e885b44f
yanc21 <ychen.0931@gmail.com>
2014-10-06 15:33:31 -0600

    need to change personal section

--- .tmp/concat/scripts/scripts.js
+++ .tmp/concat/scripts/scripts.js
@@ -47,7 +47,11 @@ angular.module('myPageApp').filter('slice', function () {
   return function (arr, start, end) {
     return (arr || []).slice(start, end);
   };
-});
+}).filter('compareFilter', [function () {
+    return function (input, compareValue, trueValue, falseValue) {
+      return input === compareValue ? trueValue : falseValue;
+    };
+  }]);
 /**
  * Created by yanc21 on 7/31/14.
  */
